706d2f2de52211b26ae4613fe53dc9ea62f4538b
Thiago <thiagowirzdemoura@gmail.com>
2022-03-17 19:28:11 -0300

    feat: get shortUrl

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -32,7 +32,7 @@ export async function getShortUrl(req, res) {
 
   try {
     const result = await connection.query(
-      'SELECT s.id, s.shortUrl, s.url FROM shortUrls s WHERE "shortUrl"= $1',
+      'SELECT s.id, s."shortUrl", s.url FROM shortUrls s WHERE "shortUrl"= $1',
       [shortUrl]
     );
 
